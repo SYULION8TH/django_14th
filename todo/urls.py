d6c8d651dc862ad92c6eabefb1d8613d1eddcfa3
sneakstarberry <gihong2012@likelion.org>
2020-08-09 11:43:32 +0900

    complete

--- todo/urls.py
+++ todo/urls.py
@@ -5,11 +5,10 @@ from todo import views
 # Default Router 사용 X ==> API ROOT 없음. 
 
 urlpatterns = [
-    # 127.0.0.1:8000/post == ListView
+    # 127.0.0.1:8000/todo == ListView
     path('todo/', views.TodoListGenerics.as_view()),  
-    # 127.0.0.1:8000/post/<pk> == DetailView
+    # 127.0.0.1:8000/todo/<pk> == DetailView
     path('todo/<int:pk>/', views.TodoDetailGenerics.as_view()), 
 ]
 
-#
-# urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
+urlpatterns = format_suffix_patterns(urlpatterns)
