d7681047507bdeeefad26eeaa81db48401666f2c
sneakstarberry <gihong2012@likelion.org>
2020-08-19 00:29:59 +0900

    apiviews

--- todo/views.py
+++ todo/views.py
@@ -1,34 +1,46 @@
-# 데이터 처리 대상 : 모델, Serializer import 시키기
 from .models import Todo
 from .serializers import TodoSerializer
 
-from rest_framework import generics
-from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 
+class TodoListAPIView(APIView):
+    def get(self, request):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
 
-class TodoListMixins(mixins.ListModelMixin, mixins.CreateModelMixin, 
-                generics.GenericAPIView):
-    queryset = Todo.objects.all()   # 쿼리셋 등록!
-    serializer_class = TodoSerializer # Serializer 클래스 등록!
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class TodoDetailMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, 
-                mixins.DestroyModelMixin, generics.GenericAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+# TodoList 클래스와는 달리 pk값을 받음 (메소드에 pk인자)
+class TodoDetailAPIView(APIView):
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    def get(self, request, pk, format=None):
+        
+        todo = self.get_object(pk)
+        # post = get_object_or_404(Post, pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, pk, format=None):
+        todo = self.get_object(pk)
+        serializer = PostSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        todo = self.get_object(pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
