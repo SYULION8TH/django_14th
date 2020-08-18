88f5020d129cefae71976e5da070b6979706cd31
sneakstarberry <gihong2012@likelion.org>
2020-08-19 00:36:00 +0900

    commit

--- todo/urls.py
+++ todo/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('todo/', views.TodoListMixins.as_view()), # 라우터에 등록된 url들을 include 해준다.
-    path('todo/<int:pk>/', views.TodoDetailMixins.as_view()), # 라우터에 등록된 url들을 include 해준다.
+    path('todo/', views.TodoListAPIView.as_view()), # 라우터에 등록된 url들을 include 해준다.
+    path('todo/<int:pk>/', views.TodoDetailAPIView.as_view()), # 라우터에 등록된 url들을 include 해준다.
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
